af9c96d9827da58fd633ce8c485ea28f46e5f7d8
Vitali <usik.vitaly@gmail.com>
2015-11-12 16:11:06 +0300

    update cc_db.sql only for testing git

--- src/main/webapp/resources/css/cc_db.sql
+++ src/main/webapp/resources/css/cc_db.sql
@@ -4,7 +4,7 @@
 -- Server OS:                    Win64
 -- HeidiSQL Version:             9.3.0.4984
 -- --------------------------------------------------------
-
+/* only for testing git */
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
